d69b4ca5e72163b86eb0afa085e777f45fa1686d
Thomas Carter <thomas.carter@appliedinnovationcorp.com>
2025-07-23 07:46:32 -0400

    Update lumache.py

--- lumache.py
+++ lumache.py
@@ -1,23 +1,17 @@
 """
-Lumache - Python library for cooks and food lovers.
+AIC AIPaas - AIPaaS for AIC
 """
 
 __version__ = "0.1.0"
 
 
-class InvalidKindError(Exception):
-    """Raised if the kind is invalid."""
+class Error(Exception):
+    """Raised if there is error."""
     pass
 
 
-def get_random_ingredients(kind=None):
+def hello_world():
     """
-    Return a list of random ingredients as strings.
-
-    :param kind: Optional "kind" of ingredients.
-    :type kind: list[str] or None
-    :raise lumache.InvalidKindError: If the kind is invalid.
-    :return: The ingredients list.
-    :rtype: list[str]
+    Return a 'hello world'
     """
-    return ["shells", "gorgonzola", "parsley"]
+    return "Hello World!"
